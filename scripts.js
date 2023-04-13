dd218df5e053b54becc4bc90a08505e2630bb54d
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-13 16:25:43 +0200

    edits

--- scripts.js
+++ scripts.js
@@ -1,56 +1,54 @@
 // scripts.js
 
 const data = {
-    lists: [
-      ['first', [15, 11, 13, 7, 5]],
-      ['second', [2, 6, 8, 4, 14, 12, 10]],
-      ['third', [9, 3, 1]],
-    ],
-  };
-  
-  // Only edit below
-  
-  const [firstList, secondList, thirdList] = data.lists;
-  
-  const result = [];
-  
-  const extractBiggest = () => {
-    const lastOfFirst = firstList[1][firstList[1].length - 1];
-    const lastOfSecond = secondList[1][secondList[1].length - 1];
-    const lastOfThird = thirdList[1][thirdList[1].length - 1];
-  
-    if (lastOfFirst >= lastOfSecond && lastOfFirst >= lastOfThird) {
-      return firstList[1].pop();
-    }
-  
-    if (lastOfSecond >= lastOfFirst && lastOfSecond >= lastOfThird) {
-      return secondList[1].pop();
-    }
+	lists: [
+		['first', [15, 11, 13, 7, 5]],
+		['second', [2, 6, 8, 4, 14, 12, 10]],
+		['third', [9, 3, 1]],
+	],
+};
 
-    
-    return thirdList[1].pop();
-  };
+// Only edit below
+
+const [firstList, secondList, thirdList] = data.lists;
+
+const result = [];
+
+const extractBiggest = () => {
+const lastOfFirst = firstList[1][firstList[1].length - 1];
+const lastOfSecond = secondList[1][secondList[1].length - 1];
+const lastOfThird = thirdList[1][thirdList[1].length - 1];
+
+if (lastOfFirst >= lastOfSecond && lastOfFirst >= lastOfThird) {
+return firstList[1].pop();
+}
+
+if (lastOfSecond >= lastOfFirst && lastOfSecond >= lastOfThird) {
+return secondList[1].pop();
+}
 
 
-  // Only edit above
-  
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  
-  console.log(result);
-  
+return thirdList[1].pop();
+}
+
+// Only edit above
+
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+
+console.log(result);
